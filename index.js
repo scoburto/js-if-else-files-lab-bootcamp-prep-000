056912950ccd8d2f8e54e15c77609f059c9e1590
scoburto@gmail.com <scoburto@gmail.com>
2018-03-03 22:56:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ if (name === "Susan") {
   console.log("The name is correct")
 }
 else {
-  console.log("Expected the name to be Susan.")
+  console.log("Expected the name to be Susan")
 }
 // Don't worry about this
 module.exports = { name, height
